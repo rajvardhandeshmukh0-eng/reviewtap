09027dff8eaff00a0cba5f026fb875d702d3bf38
rajvardhandeshmukh0-eng <rajvardhandeshmukh0@gmail.com>
2026-09-10 09:16:59 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const config = {
+
+    businessName: "ABC Restaurant",
+
+    googleReviewUrl: "PASTE_GOOGLE_REVIEW_LINK_HERE",
+
+    suggestions: [
+
+        "Great experience! The service was excellent and the staff were very friendly. Highly recommended!",
+
+        "Really happy with the service. The staff were helpful and professional. I would definitely recommend this place.",
+
+        "Excellent service and a very good overall experience. Friendly staff and great support!"
+
+    ]
+
+};
